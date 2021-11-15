1cb18fbe86c9a5aaf449a8a6c7fd57c6ef33b6a5
Koramit Pichanaharee <koramit.p@gmail.com>
2021-11-15 20:29:04 +0700

    EP 03

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,4 +1,16 @@
 <template>
     <h1 class=" text-2xl text-red-400">Hello</h1>
     <h2 class=" text-xl text-blue-500 border border-black">Home Dad Bakery</h2>
+
+    <div class="grid gap-2 md:grid-cols-2 md:gap-4 xl:grid-cols-3 xl:gap-6 p-2 md:p-4 xl:p-6">
+        <div class="rounded-lg border-2 p-2 ">Lorem ipsum dolor sit amet.</div>
+        <div class="rounded-lg border-2 p-2 ">Expedita nobis at harum voluptates.</div>
+        <div class="rounded-lg border-2 p-2 ">At repellat exercitationem ratione illo?</div>
+        <div class="rounded-lg border-2 p-2 ">Officiis aut excepturi deleniti iste.</div>
+        <div class="rounded-lg border-2 p-2 ">Asperiores magni dolor veniam et.</div>
+        <div class="rounded-lg border-2 p-2 ">Exercitationem hic quibusdam tenetur quasi.</div>
+        <div class="rounded-lg border-2 p-2 ">Iure quis quas dignissimos officiis!</div>
+        <div class="rounded-lg border-2 p-2 ">Praesentium quisquam enim totam nesciunt.</div>
+        <div class="rounded-lg border-2 p-2 ">Explicabo eligendi nemo rem porro?</div>
+    </div>
 </template>
